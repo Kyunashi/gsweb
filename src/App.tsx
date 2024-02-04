1faa4bed6d5bd8e57ff4dc4aef5a2f07c6be44cc
quentin <qlauterbach.dev@gmail.com>
2024-02-05 00:18:30 +0100

    Ceanup of unneeded files

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a> Hello World
-        </a>
-      </header>
+    Hello World
     </div>
   );
 }
