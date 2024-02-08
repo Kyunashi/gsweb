3ca516e5910fe9c0cd78b752302c3089ef9fb825
quentin <qlauterbach.dev@gmail.com>
2024-02-08 01:50:11 +0100

    Add backend login request

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,24 +4,41 @@ const LoginForm: React.FC = () => {
     const [username, setUsername] = React.useState('');
     const [password, setPassword] = React.useState('');
 
+    async function postData(url = "", data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            headers: {
+                "Content-Type": "application/json",
+                // "Accept-Encoding": "gzip, deflate, br",
+                // "Access-Control-Allow-Origin": "*",
+                // "Connection": "keep-alive"
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        });
+        return response.json(); // parses JSON response into native JavaScript objects
+    }
+
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         console.log('username: ', username);
         console.log('password: ', password);
+        console.log(JSON.stringify({
+            "username": username,
+            "password": password,
+        }))
+         postData('http://localhost:8080/api/auth/login', {
+            "username": username,
+            "password": password,
+        }).then((data) => {
+            console.log(data);
+         })
+
         // if login success -> redirect to home oder so
-        fetch('localhost:8080/api/auth/login', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password,
-            })
-        }).then(function (response) {
-            console.log(response)
-        })
+
     };
     return (
         <form onSubmit={handleSubmit}>
