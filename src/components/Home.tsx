8b594f00dc83ae2981ad1ed03f41578acdcd4538
quentin <qlauterbach.dev@gmail.com>
2024-03-18 21:11:07 +0100

    Add useRef for stompClient

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,27 +1,25 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {Client} from "@stomp/stompjs";
 
 const Home: React.FC = () => {
 
     const [isConnected, setIsConnected] = React.useState(false);
-    var greeting;
+    const [greeting, setGreeting] = React.useState('');
+    const stompClientRef = useRef<Client | null>(null);
 
-    React.useEffect(() => {
-        connect();
-    }, [])
-
-    const stompClient = new Client({
-        brokerURL: 'ws://192.168.178.42:8080/portfolio'
-    });
-
+    if (!stompClientRef.current) {
+        stompClientRef.current = new Client({
+            brokerURL: 'ws://192.168.178.42:8080/create'
+        });
+    }
+    const stompClient = stompClientRef.current;
 
     stompClient.onConnect = (frame) => {
         setIsConnected(true);
-        console.log('Connected' + frame);
+        console.log('Connected: ' + frame);
         stompClient.subscribe('/room/greetings', (greeting) => {
-            greeting = JSON.parse(greeting.body).content;
-        })
-
+            setGreeting(JSON.parse(greeting.body).content);
+        });
     }
 
     stompClient.onWebSocketError = (error) => {
@@ -34,53 +32,77 @@ const Home: React.FC = () => {
     };
 
 
+
+
     function connect() {
+
         if(!isConnected) {
             console.log("connecting")
+            // @ts-ignore
             stompClient.activate();
         }
     }
 
-    function disconnet() {
-        stompClient.deactivate();
-        setIsConnected(false);
-        console.log('Disconnected')
+    function disconnect() {
+
+        console.log("disconnecting")
+        // @ts-ignore
+        stompClient.deactivate()
+            .then(value => {
+                setIsConnected(false);
+                setGreeting('');
+                console.log('Disconnected');
+
+            })
     }
 
-    React.useEffect(() => {
-
-    }, [])
 
 
     const handleCreateRoom = () => {
-        console.log("Clicked")
+
         if(isConnected){
-            disconnet();
+
+            disconnect();
             return;
         }
-        console.log("trying to connect")
+        // @ts-ignore
+        stompClient.configure({brokerURL: 'ws://192.168.178.42:8080/create'})
         connect();
     }
 
     const handleJoinRoom = () => {
-        sendMessage();
+
+        if(isConnected){
+            disconnect();
+            return;
+        }
+        // @ts-ignore
+        stompClient.configure({brokerURL: 'ws://192.168.178.42:8080/join'})
+        connect();
     }
 
     const sendMessage = () => {
+
+        // @ts-ignore
         stompClient.publish({
-            destination: "app/app/",
+            destination: "/websocket/hi/",
             body: JSON.stringify({'name' : "Kyew"})
         })
+
     }
 
 
-
+    const handleSend = () => {
+        sendMessage()
+    }
     return (
       <div>
-          <button className={'joinbtn'} onClick={handleCreateRoom}>join rooom</button>
-          <button className={'createbtn'} onClick={handleJoinRoom}>create room</button>
+          <button className={'joinbtn'} onClick={handleJoinRoom}>join rooom</button>
+          <button className={'createbtn'} onClick={handleCreateRoom}>create room</button>
+          <button className={'sendbtn'} onClick={handleSend}>send</button>
           {isConnected ? <label>Connected :D</label> : <label>Disconnected :(</label>}
           <label>{greeting}</label>
+          <label> </label>
       </div>
     );
 }
